200cf22af622114a22fa05f2a03255528cecde87
jeffersonAG <gutierrez22754@gmail.com>
2023-10-31 21:20:40 -0600

    login2.0
    ventana para iniciar sesión

--- app/src/main/java/com/example/carpooling/MainActivity.kt
+++ app/src/main/java/com/example/carpooling/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.carpooling
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+/* se crea un boton para entrar  al apartado  Log in  , esto para poder iniciar secion en caso
+* de que haya una cuanta registrada en la app */
+        val btn: Button = findViewById(R.id.button)
+        btn.setOnClickListener {
+            val intent : Intent = Intent(this , log_in::class.java)
+            startActivity(intent)
+        }
+
     }
 }
