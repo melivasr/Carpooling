095acce20e650a46eda6d3b3e241ee7c4c95b311
KendallFdez <kenfernandez204@gmail.com>
2023-11-14 04:38:16 -0600

    mapa funcional

---
+++ app/src/main/java/com/example/carpooling/MapaActivity.kt
@@ -0,0 +1,42 @@
+package com.example.carpooling
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+
+class MapaActivity: AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var map:GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.mapa)
+        createFragment()
+    }
+    private fun createFragment(){
+        val mapFragment: SupportMapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map=googleMap
+        createMarker()
+    }
+    private fun createMarker(){
+        val coordinates = LatLng(9.855202539981171, -83.90903677311904)
+        val marker = MarkerOptions().position(coordinates).title("F2")
+        map.addMarker(marker)
+        map.animateCamera(
+            CameraUpdateFactory.newLatLngZoom(coordinates,18f),
+            4000,
+            null
+        )
+    }
+}
