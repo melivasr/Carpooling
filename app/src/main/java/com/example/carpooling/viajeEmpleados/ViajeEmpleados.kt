39b31a9c861041d4e657e0155abbf7cae59aa06b
melivasr <anavasquez@estudiantec.cr>
2023-11-15 11:54:24 -0600

    ventana

---
+++ app/src/main/java/com/example/carpooling/viajeEmpleados/ViajeEmpleados.kt
@@ -0,0 +1,12 @@
+package com.example.carpooling.viajeEmpleados
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.carpooling.R
+
+class ViajeEmpleados : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_empleados)
+    }
+}
