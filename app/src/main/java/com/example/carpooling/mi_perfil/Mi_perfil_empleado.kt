262764192fe904620f252ca2f119bcb9a84aaa80
jeffersonAG <gutierrez22754@gmail.com>
2023-11-15 13:07:53 -0600

    botón cerrar sesión documentado
    botón de cerrar sesión  documentado

--- app/src/main/java/com/example/carpooling/mi_perfil/Mi_perfil_empleado.kt
+++ app/src/main/java/com/example/carpooling/mi_perfil/Mi_perfil_empleado.kt
@@ -7,8 +7,16 @@ import android.widget.Button
 import android.widget.TextView
 import com.example.carpooling.MainActivity
 import com.example.carpooling.R
-
+/**
+ * La clase [Mi_Perfil_Empleado] representa la actividad del perfil del empleado.
+ * Permite al empleado cerrar sesión al hacer clic en el botón correspondiente.
+ */
 class Mi_perfil_empleado : AppCompatActivity() {
+    /**
+     * Método llamado cuando la actividad se crea.
+     *
+     * @param savedInstanceState Datos que pueden ser utilizados para restablecer el estado de la actividad.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mi_perfil_empleado)
@@ -22,14 +30,17 @@ class Mi_perfil_empleado : AppCompatActivity() {
             finish() // Cerrar la actividad actual después de iniciar sesión
         }
 
+        // Obtener referencias a los elementos de la interfaz de usuario
         val textViewNombre = findViewById<TextView>(R.id.textViewNombre)
         val textViewCorreo = findViewById<TextView>(R.id.textViewCorreo)
         val textViewCalificacion = findViewById<TextView>(R.id.textViewCalificacion)
 
+        // Obtener datos del intent
         val nombre = intent.getStringExtra("nombre")
         val correo = intent.getStringExtra("email")
         val calificacion = intent.getStringExtra("calificacion")
 
+        // Establecer los datos en los elementos de la interfaz de usuario
         textViewNombre.text = "Nombre: $nombre"
         textViewCorreo.text = "Correo: $correo"
         textViewCalificacion.text = "Calificación: $calificacion"
