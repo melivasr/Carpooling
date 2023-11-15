2940611d928748ca0a98c3a3cb38dabc634e71d7
jeffersonAG <gutierrez22754@gmail.com>
2023-11-15 12:58:29 -0600

    botón cerrar sesión
    botón de cerrar sesión para usuario y conductor

--- app/src/main/java/com/example/carpooling/mi_perfil/Mi_perfil_conductor.kt
+++ app/src/main/java/com/example/carpooling/mi_perfil/Mi_perfil_conductor.kt
@@ -1,8 +1,11 @@
 package com.example.carpooling.mi_perfil
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
+import com.example.carpooling.MainActivity
 import com.example.carpooling.R
 
 
@@ -10,6 +13,14 @@ class Mi_perfil_conductor : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mi_perfil_conductor)
+        // Configurar el botón para cerrar sesión
+        val btnCerrarSesion: Button = findViewById(R.id.buttonCerrarSesion)
+        btnCerrarSesion.setOnClickListener {
+            // Iniciar la actividad de inicio de sesión al hacer clic en el botón
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish() // Cerrar la actividad actual después de iniciar sesión
+        }
 
         val correo = intent.getStringExtra("email")
         val nombre = intent.getStringExtra("nombre")
--- app/src/main/java/com/example/carpooling/mi_perfil/Mi_perfil_empleado.kt
+++ app/src/main/java/com/example/carpooling/mi_perfil/Mi_perfil_empleado.kt
@@ -1,14 +1,26 @@
 package com.example.carpooling.mi_perfil
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
+import com.example.carpooling.MainActivity
 import com.example.carpooling.R
 
 class Mi_perfil_empleado : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mi_perfil_empleado)
+
+        // Configurar el botón para cerrar sesión
+        val btnCerrarSesion: Button = findViewById(R.id.buttonCerrarSesion)
+        btnCerrarSesion.setOnClickListener {
+            // Iniciar la actividad de inicio de sesión al hacer clic en el botón
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish() // Cerrar la actividad actual después de iniciar sesión
+        }
 
         val textViewNombre = findViewById<TextView>(R.id.textViewNombre)
         val textViewCorreo = findViewById<TextView>(R.id.textViewCorreo)
@@ -22,6 +34,4 @@ class Mi_perfil_empleado : AppCompatActivity() {
         textViewCorreo.text = "Correo: $correo"
         textViewCalificacion.text = "Calificación: $calificacion"
     }
-
-
 }
