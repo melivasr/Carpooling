39412e9c69d6e60c5a6aacab72bdea11a470b00b
jeffersonAG <gutierrez22754@gmail.com>
2023-11-16 12:06:42 -0600

    añadir amigos(beta1)
    función beta de añadir amigo

---
+++ app/src/main/java/com/example/carpooling/Amigos/UsuarioAdapter.kt
@@ -0,0 +1,43 @@
+package com.example.carpooling.Amigos
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.carpooling.R
+class UsuarioAdapter(private val usuarios: List<String>, private val listener: UsuarioItemClickListener) :
+    RecyclerView.Adapter<UsuarioAdapter.UsuarioViewHolder>() {
+
+    interface UsuarioItemClickListener {
+        fun onAccionButtonClick(usuario: String)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UsuarioViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.activity_anadir_amigos, parent, false)
+        return UsuarioViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: UsuarioViewHolder, position: Int) {
+        val usuario = usuarios[position]
+        holder.bind(usuario, listener)
+    }
+
+    override fun getItemCount(): Int {
+        return usuarios.size
+    }
+
+    class UsuarioViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val textViewNombreUsuario: TextView = itemView.findViewById(R.id.textViewNombreUsuario)
+        private val botonAccion: Button = itemView.findViewById(R.id.botonAccion)
+
+        fun bind(usuario: String, listener: UsuarioItemClickListener) {
+            textViewNombreUsuario.text = usuario
+
+            botonAccion.setOnClickListener {
+                listener.onAccionButtonClick(usuario)
+            }
+        }
+    }
+}
